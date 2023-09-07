45236db4a7f65f121b6aab12413437801bbc890b
Ghulam Abbas <144353898+GhulamAbbas23i-6061@users.noreply.github.com>
2023-09-07 07:27:03 -0700

    My first commit

---
+++ Programs/Average_program.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std ;
+int main()
+{
+float  n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,average;
+cin>>  n1 ;
+cin>>  n2 ;
+cin>>  n3 ;
+cin>>  n4 ;
+cin>>  n5 ;
+cin>>  n6 ;
+cin>>  n7 ;
+cin>>  n8 ;
+cin>>  n9 ;
+cin>>  n10 ;
+
+average=( n1+n2+n3+n4+n5+n6+n7+n8+n9+n10)/10 ;
+
+cout<< average ;
+}
+
